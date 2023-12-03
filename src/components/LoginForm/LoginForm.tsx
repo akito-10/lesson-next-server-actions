90a48da7d1f5169d68e97d7fe58a59b095448b59
github akito-10 <akt.fukuda@gmail.com>
2023-12-04 00:19:24 +0900

    それっぽいデザインの追加

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -2,18 +2,45 @@
 
 import { login } from "./actions";
 
-export default function LoginForm() {
+export const LoginForm = () => {
   return (
-    <form action={login}>
-      <label>
-        Name:
-        <input type="text" name="name" required />
-      </label>
-      <label>
-        Password:
-        <input type="password" name="password" required />
-      </label>
-      <button type="submit">Log in</button>
+    <form action={login} className="bg-white px-8 pt-6 pb-8 mb-4">
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="username"
+        >
+          Name:
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          type="text"
+          name="name"
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="password"
+        >
+          Password:
+        </label>
+        <input
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          type="password"
+          name="password"
+          required
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Log in
+        </button>
+      </div>
     </form>
   );
-}
+};
