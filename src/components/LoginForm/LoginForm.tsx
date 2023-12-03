43e5b8f784f86536deb69552d7009623ae6baa50
github akito-10 <akt.fukuda@gmail.com>
2023-12-04 00:22:12 +0900

    ボタンを横幅いっぱいに

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -35,7 +35,7 @@ export const LoginForm = () => {
       </div>
       <div className="flex items-center justify-between">
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
         >
           Log in
